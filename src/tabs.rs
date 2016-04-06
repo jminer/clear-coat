00dd07d6f779dedc9a93e5ebaf6f543ad007d08b
Jordan Miner <jminer7@gmail.com>
2016-04-06 00:34:06 -0500

    Add very basic Tabs

---
+++ src/tabs.rs
@@ -0,0 +1,56 @@
+/* Copyright 2016 Jordan Miner
+ *
+ * Licensed under the MIT license <LICENSE or
+ * http://opensource.org/licenses/MIT>. This file may not be copied,
+ * modified, or distributed except according to those terms.
+ */
+
+use super::control_prelude::*;
+use super::containers::{
+    Container,
+    NonDialogContainer,
+};
+
+#[derive(Clone)]
+pub struct TabInfo<'a, 'b> {
+    title: &'a str,
+    control: &'b Control,
+}
+
+#[derive(Clone)]
+pub struct Tabs(HandleRc);
+
+impl Tabs {
+    pub fn new() -> Self {
+        unsafe {
+            ::iup_open();
+            let ih = IupTabsv(ptr::null_mut());
+            Tabs(HandleRc::new(ih))
+        }
+    }
+
+    pub fn tab_title(&self, pos: usize) -> String {
+        get_str_attribute(self.handle(), &format!("TABTITLE{}\0", pos))
+    }
+
+    pub fn set_tab_title(&self, pos: usize, title: &str) -> &Self {
+        set_str_attribute(self.handle(), &format!("TABTITLE{}\0", pos), title);
+        self
+    }
+
+    pub fn add_tabs(&self, info: &[TabInfo]) -> &Self {
+        let mut index = self.child_count();
+        for ti in info {
+            self.append(ti.control).expect("failed to append tab");
+            self.set_tab_title(index, ti.title);
+            index += 1;
+        }
+        self
+    }
+}
+
+impl_control_traits!(Tabs);
+
+impl Container for Tabs {}
+impl NonDialogContainer for Tabs {}
+
